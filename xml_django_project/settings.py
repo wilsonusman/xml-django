0b5a76d95fa8c1118807c725a8f7c073d9cc7ec3
Wilson Usman <usmanwilson@gmail.com>
2024-06-10 21:11:43 -0400

    Add file upload functionality

--- xml_django_project/settings.py
+++ xml_django_project/settings.py
@@ -32,7 +32,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv("DEBUG", "False") == "True"
 DEBUG = True
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost,.ondigitalocean.app').split(',')
 
